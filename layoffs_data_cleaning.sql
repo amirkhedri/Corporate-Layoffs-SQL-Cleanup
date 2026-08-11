a87dab2726d7cbd594417b8317dd2186436a74da
Amir Khedri <khedria95@gmail.com>
2026-08-11 12:28:33 +0330

    Identifying the Dups

--- layoffs_data_cleaning.sql
+++ layoffs_data_cleaning.sql
@@ -5,8 +5,27 @@ select *from layoffs;
 -- 3. Null Values or Blank values
 -- 4 . Remove Any Columns
 
+
+
+-- Creating the Staging Table
 Create Table layoffs_staging 
 Like layoffs;
 insert layoffs_staging
 select *from layoffs;
-select *from layoffs_staging
+select *from layoffs_staging;
+
+
+-- 1.Removing Duplicates
+select *
+, Row_Number() over (partition by stage,company , location, funds_raised_millions,  industry , total_laid_off , country, percentage_laid_off , `date`) as "row_num"
+from layoffs_staging;
+with duplicate_cte as 
+(
+select *
+, Row_Number() over (partition by stage ,company ,  industry , total_laid_off , country, percentage_laid_off , `date`) as "row_num"
+from layoffs_staging
+)
+select * from duplicate_cte 
+where row_num >= 2 ;
+
+
