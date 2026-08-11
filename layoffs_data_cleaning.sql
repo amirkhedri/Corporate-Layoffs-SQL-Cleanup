030a01b2383747f49725174279e913886bf34158
Amir Khedri <khedria95@gmail.com>
2026-08-11 11:56:00 +0330

    Creating the layoffsstaging table

--- layoffs_data_cleaning.sql
+++ layoffs_data_cleaning.sql
@@ -1,7 +1,9 @@
 -- Data Cleaning
-select *from layoffs
+select *from layoffs;
 -- 1.Removing Duplicates
 -- 2. Standardize the Data
 -- 3. Null Values or Blank values
 -- 4 . Remove Any Columns
 
+Create Table layoffs_staging 
+Like lay_offs;
