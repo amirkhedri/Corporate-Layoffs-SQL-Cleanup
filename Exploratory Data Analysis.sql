2c02f8dd775feb245b3f5d9cbb95084d23165fe4
Amir Khedri <khedria95@gmail.com>
2026-08-15 11:10:17 +0330

    FInding total laid offs for each company

--- Exploratory Data Analysis.sql
+++ Exploratory Data Analysis.sql
@@ -3,6 +3,12 @@
 select max(total_laid_off) , max(percentage_laid_off) from layoffs_staging2;
 select * from layoffs_staging2
 where percentage_laid_off=1
-order by total_laid_off desc;
+order by total_laid_off desc;	
+
+
+
+select company , sum(total_laid_off) from layoffs_staging2 
+group by company
+order by 2 desc;
 
 
