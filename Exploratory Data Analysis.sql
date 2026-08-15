558ac8074c156c2e71d23f223ff0475ee04e3daf
Amir Khedri <khedria95@gmail.com>
2026-08-15 11:04:44 +0330

    sorting by most laid offs per company

--- Exploratory Data Analysis.sql
+++ Exploratory Data Analysis.sql
@@ -2,6 +2,7 @@
 
 select max(total_laid_off) , max(percentage_laid_off) from layoffs_staging2;
 select * from layoffs_staging2
-where percentage_laid_off=1;
+where percentage_laid_off=1
+order by total_laid_off desc;
 
 
