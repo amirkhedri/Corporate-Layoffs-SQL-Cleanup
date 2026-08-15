daf8a637f5fa6c52d892644cf563beefdb9c8635
Amir Khedri <khedria95@gmail.com>
2026-08-15 11:27:56 +0330

    calculating each year laid offs

--- Exploratory Data Analysis.sql
+++ Exploratory Data Analysis.sql
@@ -20,3 +20,8 @@ order by 2 desc;
 select country , sum(total_laid_off) from layoffs_staging2 
 group by country
 order by 2 desc;
+
+select year(`date`) , sum(total_laid_off) 
+from layoffs_staging2
+group by YEAR(`date`)
+order by 1;
