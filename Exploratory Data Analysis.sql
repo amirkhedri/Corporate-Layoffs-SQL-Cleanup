0557ee6240e3cb3c8158ba202f5fb0c6874ee214
Amir Khedri <khedria95@gmail.com>
2026-08-15 11:17:54 +0330

    Finding the most laid off countries and sorting based on it

--- Exploratory Data Analysis.sql
+++ Exploratory Data Analysis.sql
@@ -1,11 +1,12 @@
 -- Exploratory Data Analysis (EDA)
 
+-- Finding fully laid off companies 
 select max(total_laid_off) , max(percentage_laid_off) from layoffs_staging2;
 select * from layoffs_staging2
 where percentage_laid_off=1
 order by total_laid_off desc;	
 
-
+-- Finding total laid offs for each parameter
 
 select company , sum(total_laid_off) from layoffs_staging2 
 group by company
@@ -16,4 +17,6 @@ group by industry
 order by 2 desc;
 
 
-
+select country , sum(total_laid_off) from layoffs_staging2 
+group by country
+order by 2 desc;
